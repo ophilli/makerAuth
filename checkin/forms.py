d3ad055814e981571ac1119e79d3a7c719c1a72c
Owen Phillips <ophilli@clemson.edu>
2017-09-05 13:17:14 -0400

    Rudimentary RFID Login

--- checkin/forms.py
+++ checkin/forms.py
@@ -7,3 +7,6 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ('username', 'cuid', 'rfid', 'first_name', 'last_name')
+
+class CheckinForm(forms.Form):
+    rfid = forms.CharField(label='RFID', max_length=11) # tied to models.User.rfid
--- checkin/views.py
+++ checkin/views.py
@@ -3,17 +3,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
 from .models import Certification, User, Event
-from .forms import UserForm
+from .forms import UserForm, CheckinForm
 
 def index(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = CheckinForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            rfid = form.cleaned_data['rfid']
+            userkey = User.objects.get(rfid=rfid)
+            Event.objects.create(user=userkey, status = 'SE')
+            return redirect('/checkin/')
 
-    return HttpResponse("Hello, world. You're at the polls index.")
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = CheckinForm()
+        return render(request, 'checkin/user_new.html', {'form': form})
 
 def user_new(request):
     if request.method =="POST":
         form = UserForm(request.POST)
         if form.is_valid():
-            post = form.save(commit=True)
+            post = form.save(commit=False)
             return redirect('index')
     else:
         form = UserForm()
