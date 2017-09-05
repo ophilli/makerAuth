55074a86e2f9d526078505191cd612a395bd2e98
Owen Phillips <ophilli@clemson.edu>
2017-09-05 10:29:50 -0400

    Implement basic events list

--- checkin/views.py
+++ checkin/views.py
@@ -2,5 +2,15 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Certification, User, Event
+
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def users(request):
+    return 0
+
+def events(request):
+    e_list = Event.objects.all()
+    context = {'event_list': e_list}
+    return render(request, 'checkin/events.html', context)
--- makerAuth/urls.py
+++ makerAuth/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$', include('checkin.urls')),
+    url(r'^checkin/', include('checkin.urls')),
     url(r'^admin/', admin.site.urls),
 ]
