d126808a05e212849ce7b9c69c9f75abcd2c6d39
Owen Phillips <ophilli@clemson.edu>
2017-09-05 01:56:03 -0400

    Add Checkin as Installed App

--- makerAuth/settings.py
+++ makerAuth/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'checkin.apps.CheckinConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
