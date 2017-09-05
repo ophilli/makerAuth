c52772fa5807d34518468271e5d2863addb4a67c
Owen Phillips <ophilli@clemson.edu>
2017-09-05 12:49:51 -0400

    New User Form

--- checkin/urls.py
+++ checkin/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^events/$', views.events),
+    url(r'^user/new/$', views.user_new, name='user_new'),
 ]
--- checkin/views.py
+++ checkin/views.py
@@ -1,14 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from django.http import HttpResponse
 
 from .models import Certification, User, Event
+from .forms import UserForm
 
 def index(request):
+
     return HttpResponse("Hello, world. You're at the polls index.")
 
-def users(request):
-    return 0
+def user_new(request):
+    if request.method =="POST":
+        form = UserForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=True)
+            return redirect('index')
+    else:
+        form = UserForm()
+    return render(request, 'checkin/user_new.html', {'form': form})
 
 def events(request):
     e_list = Event.objects.all()
